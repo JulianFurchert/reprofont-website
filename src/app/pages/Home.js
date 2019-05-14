59687f33b6bf405575936f522069d0016339774a
Julian Furchert <work@julianfurchert.de>
2019-05-14 18:44:10 +0200

    update text

--- src/app/pages/Home.js
+++ src/app/pages/Home.js
@@ -1,6 +1,5 @@
 import _ from "lodash";
 import React, { Component } from 'react';
-import animateScrollTo from 'animated-scroll-to';
 import { connect } from "react-redux";
 import { Link } from 'react-router-dom'
 import { saveScrollPosition } from "../../actions/index";
@@ -35,10 +34,10 @@ class Overview extends Component {
         <div className="container">
           <div className="about">
             <h1 className="text">
-              Die Idee zu der Schriftfamilie Repro Font entstand in Cinema4D. In Cinema4D werden Textpfade und Splines bei der Weiterverarbeitung in Geraden unterteilt. Zur Berechnung der Zwischenpunkte stehen dafür unterschiedliche Interpolationsmethoden zu Verfügung. Das heißt, die Form eines Buchstaben wird neben der Auswahl der Schrift auch von der Auswahl und Einstellungen der Interpolationsmethode bestimmt. Dieser Umstand inspirierte mich dazu ein eigene Methode zur Neuberechnung von Glyphen zu schreiben. Die Methode erzeugt Zwischenpunkte entlang des Pfades und verschiebt diese auf ein virtuelles orthogonales Raster. Als Ausgangsform dienen die Glyphen der Repro 100. Das Raster wird von Schriftschnitt zu Schriftschnitt (Repro 200-600) immer gröber, bis die daraus resultieren Zeichen nur noch schwer lesbar sind.
+            Die Repro Familie besteht aus einer Grotesk Schrift und fünf generierten Schriftschnitten. Diese sind das Produkt eines Algorithmus, welcher die Gylphpfade auf ein darunter liegendes Raster verschiebt. Das Raster wird von Schriftschnitt zu Schriftschnitt immer gröber, bis die daraus resultieren Zeichen kaum lesbar sind.
             </h1>
             <p className="text text-small">
-              <Link to="/speciem">Speciem</Link>, <Link to="/">License</Link>, <Link to="/">Github</Link>
+              <Link to="/speciem">Speciem</Link>, <a href="https://github.com/JulianFurchert/reprofont/blob/master/LICENSE.md">License</a>, <a href="https://github.com/JulianFurchert/reprofont">Source</a>
             </p>
           </div>
         </div>
