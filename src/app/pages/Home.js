e75b888e3ce32abe75d85d0f9ac5d1ff7b1c2993
Julian Furchert <work@julianfurchert.de>
2019-05-14 18:17:23 +0200

    update Home

--- src/app/pages/Home.js
+++ src/app/pages/Home.js
@@ -10,29 +10,7 @@ import Letter from '../components/Letter'
 import Card from '../components/Card'
 import DownloadList from '../components/DownloadList'
 
-// const options = {
-//   speed: 6000
-
-// };
-
 class Overview extends Component {
-
-  // componentDidMount() {
-  //   window.scrollTo(0, this.props.scrollPosition);
-
-  //   setTimeout(() => {
-  //     animateScrollTo(250, options);
-  //   }, 3000);
-
-  //   setTimeout(() => {
-  //     animateScrollTo(800, options);
-  //   }, 5000);
-
-  //   setTimeout(() => {
-  //     animateScrollTo(0, options);
-  //   }, 8000);
-
-  // }
 
   componentWillUnmount(){
     this.props.saveScrollPosition(window.scrollY);
@@ -56,7 +34,9 @@ class Overview extends Component {
       <div className="overview">
         <div className="container">
           <div className="about">
-            <h1 className="text">Repro ist eine Groteskschrift mit der DNA einer Monotype. Fünf weitere Schriftschnitte sind Reproduktionen dessen. Sie sind ein Produkt eines Algorithmus, welcher die Outlines auf ein darunter liegendes Raster verschiebt. Das Raster wird von Schnitt zu Schnitt gröber, bis das Ergebnis nur noch Form ist.</h1>
+            <h1 className="text">
+              Die Idee zu der Schriftfamilie Repro Font entstand in Cinema4D. In Cinema4D werden Textpfade und Splines bei der Weiterverarbeitung in Geraden unterteilt. Zur Berechnung der Zwischenpunkte stehen dafür unterschiedliche Interpolationsmethoden zu Verfügung. Das heißt, die Form eines Buchstaben wird neben der Auswahl der Schrift auch von der Auswahl und Einstellungen der Interpolationsmethode bestimmt. Dieser Umstand inspirierte mich dazu ein eigene Methode zur Neuberechnung von Glyphen zu schreiben. Die Methode erzeugt Zwischenpunkte entlang des Pfades und verschiebt diese auf ein virtuelles orthogonales Raster. Als Ausgangsform dienen die Glyphen der Repro 100. Das Raster wird von Schriftschnitt zu Schriftschnitt (Repro 200-600) immer gröber, bis die daraus resultieren Zeichen nur noch schwer lesbar sind.
+            </h1>
             <p className="text text-small">
               <Link to="/speciem">Speciem</Link>, <Link to="/">License</Link>, <Link to="/">Github</Link>
             </p>
