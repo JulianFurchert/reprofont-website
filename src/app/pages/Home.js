7669cba3948a4a383f09a305bce2edf5009ddf91
Julian Furchert <work@julianfurchert.de>
2019-05-30 19:07:07 +0200

    update about

--- src/app/pages/Home.js
+++ src/app/pages/Home.js
@@ -34,8 +34,7 @@ class Overview extends Component {
         <div className="container">
           <div className="about">
             <h1 className="text">
-            Die Repro Familie besteht aus einer Grotesk Schrift und fünf generierten Schriftschnitten. Diese sind das Produkt eines Algorithmus, welcher die Gylphpfade auf ein darunter liegendes Raster verschiebt. Das Raster wird von Schriftschnitt zu Schriftschnitt immer gröber, bis die daraus resultieren Zeichen kaum lesbar sind.
-            </h1>
+              Die Idee zu der Schriftfamilie Repro entstand in Cinema4D. In Cinema werden Textpfade und Splines bei der Weiterverarbeitung in Geraden unterteilt. Zur Berechnung der Zwischenpunkte stehen dafür unterschiedliche Interpolationsmethoden zu Verfügung. Das heißt, die Form eines Buchstaben wird neben der Auswahl der Schrift auch von der Auswahl und Einstellungen der Interpolationsmethode bestimmt. Dieser Umstand inspirierte mich dazu ein eigene Methode zur Neuberechnung der Glyphen zu schreiben. Die Methode erzeugt Zwischenpunkte entlang der Pfade und verschiebt diese auf ein virtuelles orthogonales Raster. Als Ausgangsform dienen die Glyphen der Repro 100. Das Raster wird von Schriftschnitt zu Schriftschnitt (Repro 200-600) immer gröber, bis die daraus resultieren Zeichen nicht mehr lesbar sind.            </h1>
             <p className="text text-small">
               <Link to="/speciem">Speciem</Link>, <a href="https://github.com/JulianFurchert/reprofont/blob/master/LICENSE.md">License</a>, <a href="https://github.com/JulianFurchert/reprofont">Source</a>
             </p>
